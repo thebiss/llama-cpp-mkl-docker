b08c94fab97a938fdb669d20850776f4ca9b11c7
Brian Bissell <bbissell@us.ibm.com>
2024-07-28 17:32:52 -0400

    chore: tidy up - no sources, variable name

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
-docker build -t bbissell/llama-cpp-mkl:b3467 --rm=false .
+export LLAMACPP_VER=b3467
+
+docker build \
+    -t bbissell/llama-cpp-mkl:${LLAMACPP_VER} \
+    -t bbissell/llama-cpp-mkl:latest \
+    --build-arg LLAMACPP_VERSION_TAG=${LLAMACPP_VER} \
+    --rm=false \
+    .
+
 docker images
 
