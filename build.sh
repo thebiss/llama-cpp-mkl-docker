128398b4cfbebc8eee0bfa357b84145c22eb6562
thebiss <bbissell@gmail.com>
2024-07-29 16:57:42 -0400

    chore: clean up build, run scripts options

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 # Default to b3467 if not specified
 LLAMACPP_VER=${LLAMACPP_VER:-b3467}
 
-docker build \
-    -t bbissell/llama-cpp-mkl:${LLAMACPP_VER} \
-    -t bbissell/llama-cpp-mkl:latest \
+docker build . \
+    --file Dockerfile \
+    --tag bbissell/llama-cpp-mkl:${LLAMACPP_VER} \
+    --tag bbissell/llama-cpp-mkl:latest \
     --build-arg LLAMACPP_VERSION_TAG=${LLAMACPP_VER} \
-    --rm=false \
-    .
+    --rm=false
--- run.sh
+++ run.sh
@@ -13,24 +13,36 @@ function _error
 ##
 ## Main
 ##
-_LLAMA_MODEL_MSG=${LLAMA_MODEL_GGUF:-"Warning: Using a default model; set LLAMA_MODEL_GGUF to override."}
-LLAMA_MODEL_GGUF=${LLAMA_MODEL_GGUF:-$HOME/dev-in-wsl/models/mistral-7b-instruct-v0.2.Q5_K_M.gguf}
+_MODEL_DEFAULT="$HOME/dev-in-wsl/models/mistral-7b-instruct-v0.2.Q5_K_M.gguf"
+
+# Accept model as param 1, or tuse the default
+[ $# -gt 0 ] && LLAMA_MODEL_GGUF=$1
+
+_LLAMA_MODEL_MSG=${LLAMA_MODEL_GGUF:-"Warning: Using a default model; run \"$(basename $0) /path/to/model.gguf\" to override."}
+LLAMA_MODEL_GGUF=${LLAMA_MODEL_GGUF:-"$_MODEL_DEFAULT"}
+
 
 echo "Using model ${LLAMA_MODEL_GGUF}"
 echo "${_LLAMA_MODEL_MSG}"
 
 
+# Relative to absolute path
+LLAMA_MODEL_GGUF=$(realpath ${LLAMA_MODEL_GGUF})
+# Validate existence
 [ -f "${LLAMA_MODEL_GGUF}" ] || error "Cannot access model file:" "${LLAMA_MODEL_GGUF}"
 
 
+# Parts, for mounting
 _LLAMA_MODEL_GGUF_FILEONLY=$(basename "$LLAMA_MODEL_GGUF")
 _LLAMA_MODEL_GGUF_DIRNAME=$(dirname "$LLAMA_MODEL_GGUF")
 
 #
 set -x
 docker run \
-    -it \
-    -v "${_LLAMA_MODEL_GGUF_DIRNAME}:/var/models:ro" \
-    -p 8080:8080 \
-    -e "LLAMA_MODEL_GGUF=${_LLAMA_MODEL_GGUF_FILEONLY}" \
-    bbissell/llama-cpp-mkl 
+    --detach \
+    --rm \
+    --volume "${_LLAMA_MODEL_GGUF_DIRNAME}:/var/models:ro" \
+    --publish 8080:8080 \
+    --env "LLAMA_MODEL_GGUF=${_LLAMA_MODEL_GGUF_FILEONLY}" \
+    --name "llama-cpp-mkl-optimized" \
+    bbissell/llama-cpp-mkl:latest
