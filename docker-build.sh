346c00f622a815ee17d4cb93110fb6c4ceacea7b
thebiss <bbissell@gmail.com>
2024-09-17 21:31:56 -0400

    feat(build): require llama.cpp version tag at build

--- docker-build.sh
+++ docker-build.sh
@@ -3,9 +3,12 @@
 # Default to b3467 if not specified
 # 19 Aug - bump to b3486 for Ollama, which includes issue #8688
 # 19 Aug - try b3599
-LLAMACPP_VER=${LLAMACPP_VER:-b3599}
+# 13 Sept - try 3751
+LLAMACPP_VER=${LLAMACPP_VER:-b3751}
 
 cd ./docker
+
+printf "\nBuilding from llamap.cpp version $LLAMACPP_VER\n\n"
 
 docker build . \
     --file Dockerfile \
