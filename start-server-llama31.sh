6b703a3ff5832041b6a2a3ad6a83bec8320dd3dd
thebiss <bbissell@gmail.com>
2024-09-18 09:01:37 -0400

    feat(startup): use default context OR env value; enable llama3.1 with small context

---
+++ start-server-llama31.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+##
+## Runs the containerized llama-3.1, with a reduced context
+##
+env LLAMA_ARG_CTX_SIZE=65535 ./start-server.sh ../models/Meta-Llama-3.1-8B-Instruct-Q5_K_M.gguf
+
+exit 0;
--- start-server.sh
+++ start-server.sh
@@ -22,33 +22,37 @@ function _error
 ##
 ## Main
 ##
-# _MODEL_DEFAULT="../models/Mistral-7B-Instruct-v0.3-Q5_K_M.gguf"  # odd behavior
-_MODEL_DEFAULT="../models/mistral-7b-instruct-v0.2.Q5_K_M.gguf"
+_MODEL_DEFAULT="../models/Mistral-7B-Instruct-v0.3-Q5_K_M.gguf"
+# _MODEL_DEFAULT="../models/mistral-7b-instruct-v0.2.Q5_K_M.gguf"
 _MODEL_MSG=""
-LLAMA_MODEL_GGUF=${LLAMA_MODEL_GGUF:-""}
 
-
-# Accept model as param 1, overriding env if set
+# Use model in param 1, if set
 [ $# -gt 0 ] && LLAMA_MODEL_GGUF=$1
+
+LLAMA_MODEL_GGUF=${LLAMA_MODEL_GGUF:-""}
 
 # if still unset, use the default
 if [ -z "${LLAMA_MODEL_GGUF}" ]; then
     LLAMA_MODEL_GGUF="${_MODEL_DEFAULT}"
-    _MODEL_MSG="${_THIS} Warning: Model not set, using default. Run \"$(basename $0) /path/to/model.gguf\" to override."
+    _MODEL_MSG="WARNING: Model not set, using default. Run \"${_THIS} /path/to/model.gguf\" to override."
 fi
 
+LLAMA_MODEL_GGUF=$(realpath ${LLAMA_MODEL_GGUF})
 
-echo "Using model ${LLAMA_MODEL_GGUF}"
 echo "${_MODEL_MSG}"
+echo "Using model ${LLAMA_MODEL_GGUF}"
 
 # Validate existence of absolute path
-LLAMA_MODEL_GGUF=$(realpath ${LLAMA_MODEL_GGUF})
 [ -f "${LLAMA_MODEL_GGUF}" ] || _error "Cannot access model file:" "${LLAMA_MODEL_GGUF}"
 
 
 # Parts, for mounting
-_LLAMA_MODEL_GGUF_FILEONLY=$(basename "$LLAMA_MODEL_GGUF")
-_LLAMA_MODEL_GGUF_DIRNAME=$(dirname "$LLAMA_MODEL_GGUF")
+_LLAMA_MODEL_GGUF_FILEONLY=$(basename "${LLAMA_MODEL_GGUF}")
+_LLAMA_MODEL_GGUF_DIRNAME=$(dirname "${LLAMA_MODEL_GGUF}")
+
+# Warn - passing context size specs
+LLAMA_ARG_CTX_SIZE=${LLAMA_ARG_CTX_SIZE:-""}
+[ -n "${LLAMA_ARG_CTX_SIZE}" ] && echo "Using context size limit from LLAMA_ARG_CTX_SIZE, ${LLAMA_ARG_CTX_SIZE}"
 
 # Run the container
 set -x
@@ -58,5 +62,6 @@ docker run \
     --volume "${_LLAMA_MODEL_GGUF_DIRNAME}:/var/models:ro" \
     --publish 8080:8080 \
     --env "LLAMA_MODEL_GGUF=${_LLAMA_MODEL_GGUF_FILEONLY}" \
+    --env "LLAMA_ARG_CTX_SIZE" \
     --name "llama-cpp-mkl-optimized" \
     bbissell/llama-cpp-mkl:latest
