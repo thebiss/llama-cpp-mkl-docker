6a9fb7cbcd0a5704f1ad01533245e033db380c8a
thebiss <bbissell@gmail.com>
2024-11-04 09:19:49 -0500

    feat(build): llamacpp version from settings file

--- docker-build-vulkan.sh
+++ docker-build-vulkan.sh
@@ -1,14 +1,8 @@
 #! /bin/bash
 
-# Default to b3467 if not specified
-# 19 Aug - bump to b3486 for Ollama, which includes issue #8688
-# 19 Aug    - try b3599
-# 13 Sept - try 3751
-# 23 Sept - try b3812
-# 24 Oct - try b3974 - better server prompt handling + more
 source ./settings.sh
 
-LLAMACPP_VER=${LLAMACPP_VER:-b3974}
+LLAMACPP_VER=${LLAMACPP_VER:-}
 
 cd ./docker
 
--- settings.sh
+++ settings.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/echo Source this instead.
+
+# LLAMACPP Version
+#
+# Default to b3467 if not specified
+# 19 Aug    - bump to b3486 for Ollama, which includes issue #8688
+# 19 Aug    - try b3599
+# 13 Sept   - try 3751
+# 23 Sept   - try b3812
+# 24 Oct    - try b3974 - better server prompt handling + more
+# 4 Nov     - try 3989 - new vulkan ops
+LLAMACPP_VER=b3989
+
+# Default model to run
 _MODEL_DEFAULT="../models/mistralai/mistral-v0.3/Mistral-7B-Instruct-v0.3-Q5_K_M.gguf"
