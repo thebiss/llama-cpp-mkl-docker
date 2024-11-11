14d3424bd897a17c422132d8e42973dc5dd78606
thebiss <bbissell@gmail.com>
2024-11-11 11:10:24 -0500

    config(llama,model): bump default version to 4049, default model to granite 3

--- settings.sh
+++ settings.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/echo Source this instead.
 
-# LLAMACPP Version
+# LLAMACPP release tag to build from
 #
-# Default to b3467 if not specified
-# 19 Aug    - bump to b3486 for Ollama, which includes issue #8688
-# 19 Aug    - try b3599
-# 13 Sept   - try 3751
-# 23 Sept   - try b3812
-# 24 Oct    - try b3974 - better server prompt handling + more
-# 4 Nov     - try 3989 - new vulkan ops
-## LLAMACPP_VER=b3989
-LLAMACPP_VER=b4027
+# prior     - Default to b3467 if not specified
+# 19 Aug    - b3486 for Ollama, which includes issue #8688
+# 19 Aug    - b3599
+# 13 Sept   - b3751
+# 23 Sept   - b3812
+# 24 Oct    - b3974 - better server prompt handling + more
+# 4 Nov     - b3989 - new vulkan ops
+# 9 Nov     - b4048 - better UI, other q6 opts
+LLAMACPP_VER=b4048
 
 
+_MODEL_ROOT="../models"
 # Default model to run
-_MODEL_DEFAULT="../models/mistralai/mistral-v0.3/Mistral-7B-Instruct-v0.3-Q5_K_M.gguf"
+## _MODEL_DEFAULT="${_MODEL_ROOT}/mistralai/mistral-v0.3/Mistral-7B-Instruct-v0.3-Q5_K_M.gguf"
+_MODEL_DEFAULT="${_MODEL_ROOT}/ibm/granite-3.0/granite-3.0-8b-instruct-Q6_K.gguf"
