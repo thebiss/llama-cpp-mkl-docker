ade96f39084138ce53842cf08ffcc8979795f84e
thebiss <bbissell@gmail.com>
2024-11-23 18:54:20 -0500

    build(gitops): remove git build scripts, unused

--- cleanup-wsl-cache.sh
+++ cleanup-wsl-cache.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Force the kernel to empty the buff/cache
+# Without this on WSL (currently kernel 5.15.153.1)
+# large files will fill the buffer OOM crashes the VM.
+
 # free -h
-echo 1 | sudo tee /proc/sys/vm/drop_caches
+echo 1 | sudo tee /proc/sys/vm/drop_caches > /dev/null
 # free -h
